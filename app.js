18b3cc3cbafc04a7b36d110ad98d87f1e838d8ac
Kamil Smolen <kamilxsmolen@gmail.com>
2018-11-21 15:19:59 +0100

    boilerplate

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const fs = require('fs')
+const cheerio = require('cheerio')
+const request = require('request')
+const app = express()
+
+
+app.get('/scrape', function(req, res) {
+
+})
+
+app.listen('8081')
+
+console.log('Magic happens on port 8081')
+exports = module.exports = app;
