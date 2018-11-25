0a135cf9b79952ed210bc48cb60c3d336da32109
Kamil Smolen <kamilxsmolen@gmail.com>
2018-11-25 17:10:19 +0100

    zbieranie kolejnych danych

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/scrape', function(req, res){
     getDataFromMain(url).then((data) => {
         var allPs = []
         for (let i = 0, p = Promise.resolve(); i < 2; i++) {
+            if(i>0)i += 4
             p = p.then(_ => getDataFromSubPage('https://www.gry-online.pl' + data[i],Math.floor((i/data.length)*10000)/100));
             allPs.push(p)
         }
@@ -26,20 +27,45 @@ function getDataFromSubPage(url, proc){
             if(!error){
                 var $ = cheerio.load(html);
                 var textx = ""
-                var json = { producent: "", wydawca: "", tagi: [], podobne: [] };
+                var json = { producent: "", wydawca: "", tagi: [], podobne: [], ocena: {wartosc: "", ilosc_ocen: ""},lapki: {w_gore: "", w_dol: ""}, wymagania_sprzetowe: [], komentarze: [] };
                 $('.S016-game-info').filter(function(){
                     var data = $(this);
                     var target
                     target = data.children().first().children().first().children().first()
                     json.producent = target.text()
                     target = data.children().eq(1).children().first().children().first()
-                    json.wydawca = target.html()
+                    json.wydawca = target.text()
                 })
                 $('#game-tags-cnt').find('a').each(function (index, el) {
-                    json.tagi.push($(this).html())
+                    json.tagi.push($(this).text())
                 })
                 $('#game-similar-cnt').find('div').children().find('p').each(function (index, el) {
-                    json.podobne.push($(this).html())
+                    json.podobne.push($(this).text())
+                })
+                $('#game-misc-cnt').children().eq(1).children().first().children().each(function (index,el) {
+                    var data = $(this);
+                    if(index === 0) json.ocena.wartosc = data.text()
+                    else if(index === 2) json.ocena.ilosc_ocen = data.text()
+                })
+                $('.dbLapUp').filter(function () {
+                    json.lapki.w_gore = $(this).text()
+                })
+                $('.dbLapDown').filter(function () {
+                    json.lapki.w_dol = $(this).text()
+                })
+                $('#game-requirements-cnt-1-c').filter(function () {
+                    var data = $(this)
+                    var dt = data.html().split('<br>')
+                    for(var i=0;i<dt.length;i++){
+                        var wrapped = { id: "", wartosc: ""}
+                        dt[i] = dt[i].split('<b>')[1]
+                        wrapped.id = dt[i].split('</b>')[0]
+                        wrapped.wartosc = dt[i].split('</b>')[1]
+                        json.wymagania_sprzetowe.push(wrapped)
+                    }
+                })
+                $('#fcontrols-14637979').filter(function () {
+                    console.log("ind: "+ $(this).html())
                 })
                 console.log("PROGRESS: "+ proc + "%")
             }
