c7299d79922f0a7512b35f6514ec0b06c7e24b24
Kamil Smolen <kamilxsmolen@gmail.com>
2018-11-25 19:47:39 +0100

    scrapowanie danych z renderowaniem każdej strony z javascriptu żeby pobrać komentarze

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app= express();
+var phantom = require('phantom')
 
 app.get('/scrape', function(req, res){
     url = 'https://www.gry-online.pl/daty-premier-gier.asp';
@@ -19,60 +20,119 @@ app.get('/scrape', function(req, res){
         });
     })
 
+
+
 })
 
 function getDataFromSubPage(url, proc){
     return new Promise(((resolve, reject) => {
-        request(url, function(error, response, html){
-            if(!error){
-                var $ = cheerio.load(html);
-                var textx = ""
-                var json = { producent: "", wydawca: "", tagi: [], podobne: [], ocena: {wartosc: "", ilosc_ocen: ""},lapki: {w_gore: "", w_dol: ""}, wymagania_sprzetowe: [], komentarze: [] };
-                $('.S016-game-info').filter(function(){
-                    var data = $(this);
-                    var target
-                    target = data.children().first().children().first().children().first()
-                    json.producent = target.text()
-                    target = data.children().eq(1).children().first().children().first()
-                    json.wydawca = target.text()
-                })
-                $('#game-tags-cnt').find('a').each(function (index, el) {
-                    json.tagi.push($(this).text())
-                })
-                $('#game-similar-cnt').find('div').children().find('p').each(function (index, el) {
-                    json.podobne.push($(this).text())
-                })
-                $('#game-misc-cnt').children().eq(1).children().first().children().each(function (index,el) {
-                    var data = $(this);
-                    if(index === 0) json.ocena.wartosc = data.text()
-                    else if(index === 2) json.ocena.ilosc_ocen = data.text()
-                })
-                $('.dbLapUp').filter(function () {
-                    json.lapki.w_gore = $(this).text()
-                })
-                $('.dbLapDown').filter(function () {
-                    json.lapki.w_dol = $(this).text()
-                })
-                $('#game-requirements-cnt-1-c').filter(function () {
-                    var data = $(this)
-                    var dt = data.html().split('<br>')
-                    for(var i=0;i<dt.length;i++){
-                        var wrapped = { id: "", wartosc: ""}
-                        dt[i] = dt[i].split('<b>')[1]
-                        wrapped.id = dt[i].split('</b>')[0]
-                        wrapped.wartosc = dt[i].split('</b>')[1]
-                        json.wymagania_sprzetowe.push(wrapped)
-                    }
-                })
-                $('#fcontrols-14637979').filter(function () {
-                    console.log("ind: "+ $(this).html())
-                })
-                console.log("PROGRESS: "+ proc + "%")
-            }
-            resolve(json)
-        })
+
+        phantom.create().then(function(ph) {
+            ph.createPage().then(function(page) {
+                page.open(url).then(function(status) {
+                    console.log("STATUS:    "+status);
+                    page.property('content').then(function(content) {
+                        resolve(retrieveDataFromHtml(content,proc))
+                        page.close();
+                        ph.exit();
+                    });
+                });
+            });
+        });
+
     }))
 }
+
+
+function retrieveDataFromHtml(html,proc){
+    var $ = cheerio.load(html);
+    var textx = ""
+    var json = { producer: "", publisher: "", tags: [], similar: [], rating: {value: "", quantity: ""},thumbs: {up: "", down: ""}, hardware_req: [], comments: [] };
+    $('.S016-game-info').filter(function(){
+        var data = $(this);
+        var target
+        target = data.children().first().children().first().children().first()
+        json.producer = target.text()
+        target = data.children().eq(1).children().first().children().first()
+        json.publisher = target.text()
+    })
+    $('#game-tags-cnt').find('a').each(function (index, el) {
+        json.tags.push($(this).text())
+    })
+    $('#game-similar-cnt').find('div').children().find('p').each(function (index, el) {
+        json.similar.push($(this).text())
+    })
+    $('#game-misc-cnt').children().eq(1).children().first().children().each(function (index,el) {
+        var data = $(this);
+        if(index === 0) json.rating.value = data.text()
+        else if(index === 2) json.rating.quantity = data.text()
+    })
+    $('.dbLapUp').filter(function () {
+        json.thumbs.up = $(this).text()
+    })
+    $('.dbLapDown').filter(function () {
+        json.thumbs.down = $(this).text()
+    })
+    $('#game-requirements-cnt-1-c').filter(function () {
+        var data = $(this)
+        var dt = data.html().split('<br>')
+        for(var i=0;i<dt.length;i++){
+            var wrapped = { id: "", value: ""}
+            dt[i] = dt[i].split('<b>')[1]
+            wrapped.id = dt[i].split('</b>')[0]
+            wrapped.value = dt[i].split('</b>')[1]
+            json.hardware_req.push(wrapped)
+        }
+    })
+    $('.kom-lay-2016-c').children().filter(function () {
+        var data = $(this)
+        if((data.attr('id')+"").startsWith('post0')) return true;
+        else return false;
+    }).each(function (index,el){
+        var data = $(this)
+        var komm = { date: "", login: "", rank: "", degree: "", contents: ""}
+        komm.date = data.children().eq(2).text()
+        var metadataTarget = data.children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('ptr')) return true;
+            else return false;
+        }).children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('ptt')) return true;
+            else return false;
+        })
+        komm.login = metadataTarget.children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('fus')) return true;
+            else return false;
+        }).text()
+        komm.rank = metadataTarget.children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('fra')) return true;
+            else return false;
+        }).text()
+        komm.degree = metadataTarget.children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('fst')) return true;
+            else return false;
+        }).text()
+
+        komm.contents = data.children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('ptr')) return true;
+            else return false;
+        }).children().filter(function () {
+            var data = $(this)
+            if((data.attr('class')+"").startsWith('ptx')) return true;
+            else return false;
+        }).text()
+        json.comments.push(komm)
+    })
+    console.log("PROGRESS: "+ proc + "%")
+    return json
+}
+
+
 
 function getDataFromMain(url){
     return new Promise(((resolve, reject) => {
